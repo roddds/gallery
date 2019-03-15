8485012a27d3ca255860f7a2a7986b3bfac0b7da
Rodrigo Deodoro <roddds@gmail.com>
2019-03-14 20:46:33 -0700

    Add new icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,11 +24,11 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // 'gatsby-plugin-offline',
   ],
-}
+};
