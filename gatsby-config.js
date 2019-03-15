65b057848862e03eacd318594c5ed6e7d2b66cd2
Rodrigo Deodoro <roddds@gmail.com>
2019-03-14 20:45:11 -0700

    Change title and description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Gallery`,
+    description: `A statically generated photo gallery.`,
     author: `@gatsbyjs`,
   },
   plugins: [
