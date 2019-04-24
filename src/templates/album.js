53a845ab7a5c1c0edffd744d4799c0d4a127c0c2
Rodrigo Deodoro <roddds@gmail.com>
2019-04-24 01:09:33 -0700

    Add page titles

--- src/templates/album.js
+++ src/templates/album.js
@@ -1,10 +1,19 @@
 import * as React from "react";
+import Case from "case";
+import { Helmet } from "react-helmet";
 import Slideshow from "../components/Slideshow";
 
 const AlbumTemplate = props => {
   const ctx = props.pageContext;
 
-  return <Slideshow {...ctx} />;
+  return (
+    <>
+      <Helmet>
+        <title>{Case.title(ctx.name)}</title>
+      </Helmet>
+      <Slideshow {...ctx} />
+    </>
+  );
 };
 
 export default AlbumTemplate;
--- src/templates/list.js
+++ src/templates/list.js
@@ -1,14 +1,24 @@
 import * as React from "react";
+import { Helmet } from "react-helmet";
 import AlbumLink from "../components/AlbumLink";
 
 const ListTemplate = props => {
   const ctx = props.pageContext;
 
-  return ctx.albums.map(album => (
+  const albums = ctx.albums.map(album => (
     <a href={album} key={album}>
       <AlbumLink album={album} photos={ctx.allPhotos[album]} />
     </a>
   ));
+
+  return (
+    <>
+      <Helmet>
+        <title>Gallery</title>
+      </Helmet>
+      {albums}
+    </>
+  );
 };
 
 export default ListTemplate;
