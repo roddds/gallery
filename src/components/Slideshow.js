3efa2565737d9fe752aa2bf62c12349d6737fab1
Rodrigo Deodoro <roddds@gmail.com>
2019-04-24 23:45:55 -0700

    Navigation helper

--- src/components/Slideshow.js
+++ src/components/Slideshow.js
@@ -1,8 +1,17 @@
 import * as React from "react";
 import { useState, useEffect } from "react";
-import posed from "react-pose";
+import posed, { PoseGroup } from "react-pose";
 
 const Img = posed.img({});
+const Header = posed.p({
+  enter: {
+    opacity: 1,
+  },
+  exit: {
+    opacity: 0,
+    transition: { duration: 5000 },
+  },
+});
 
 const leftKey = e => e.key === "ArrowLeft";
 const rightKey = e => e.key === "ArrowRight";
@@ -35,6 +44,7 @@ const useArrows = arrows => {
 function SlideShow(props) {
   const { photos } = props;
   const [current, setCurrent] = useState(0);
+  const [titleVisible, setTitleVisible] = useState(true);
 
   const n = photos.length;
   const next = () => setCurrent(c => c + 1);
@@ -46,8 +56,27 @@ function SlideShow(props) {
     up: () => window.history.back(),
   });
 
+  useEffect(() => {
+    setTimeout(() => {
+      setTitleVisible(false);
+    }, 1000);
+  });
+
   return (
     <div className="slideshow--wrapper">
+      <Header
+        pose={titleVisible ? "enter" : "exit"}
+        key="header"
+        style={{
+          position: "absolute",
+          bottom: 0,
+          left: 0,
+          right: 0,
+          color: "#ddd",
+        }}
+      >
+        use your arrow keys
+      </Header>
       <Img
         alt=""
         className="slideshow--image"
